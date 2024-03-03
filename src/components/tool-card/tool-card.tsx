56f1a58d8b49bcba35c599e3466380dc669b0122
carl-chen <ccheartszzz@gmail.com>
2024-03-04 00:11:27 +0800

    perf: optimize url link

--- src/components/tool-card/tool-card.tsx
+++ src/components/tool-card/tool-card.tsx
@@ -1,6 +1,6 @@
 import {Avatar} from "@nextui-org/react";
 import React from "react";
-import { useRouter } from 'next/navigation';
+import {useRouter} from 'next/navigation';
 
 interface Props {
     avatar?: string;
@@ -11,18 +11,20 @@ interface Props {
 
 const ToolCard: React.FC<React.PropsWithChildren<Props>> = ({title, article, url}) => {
     const router = useRouter()
-    if (url && !url.includes('http')) {
-        const location = globalThis.location || ''
-        url = location + url
-    }
     return (
         <div className="bg-gray-400 dark:border-gray-400 overflow-auto rounded-xl cursor-pointer" onClick={() => {
-            url && router.push(url)
+            if (!url) return
+            if (url.startsWith('http')) {
+                window.open(url)
+            } else {
+                router.push(url)
+            }
+
         }}>
             <div className="flex flex-col gap-2 p-4 text-sm bg-white dark:bg-slate-900 rounded-xl  m-[1px]">
                 <Avatar isBordered radius='full' src="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
                 <div className="text-base text-black dark:text-white">{title}</div>
-                <div className="text-slate-500">{article}</div>
+                <div className="text-[#b6cdef]">{article}</div>
                 <div>{url}</div>
             </div>
         </div>)
