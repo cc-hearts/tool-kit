8a507f2f7a365960f3c308433f76c982ab24f258
carl-chen <ccheartszzz@gmail.com>
2024-03-07 19:30:32 +0800

    wip: wip page-word

--- src/components/tool-card/tool-card.tsx
+++ src/components/tool-card/tool-card.tsx
@@ -7,9 +7,10 @@ interface Props {
     title?: string
     article?: string
     url?: string
+    logo?: string
 }
 
-const ToolCard: React.FC<React.PropsWithChildren<Props>> = ({title, article, url}) => {
+const ToolCard: React.FC<React.PropsWithChildren<Props>> = ({logo, title, article, url}) => {
     const router = useRouter()
     return (
         <div className="bg-gray-400 dark:border-gray-400 overflow-auto rounded-xl cursor-pointer" onClick={() => {
@@ -19,13 +20,11 @@ const ToolCard: React.FC<React.PropsWithChildren<Props>> = ({title, article, url
             } else {
                 router.push(url)
             }
-
         }}>
-            <div className="flex flex-col gap-2 p-4 text-sm bg-white dark:bg-slate-900 rounded-xl  m-[1px]">
-                <Avatar isBordered radius='full' src="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
+            <div className="flex flex-col gap-2 p-4 text-sm bg-white dark:bg-[#1B1C1D] rounded-xl  m-[1px]">
+                <Avatar isBordered radius='full' src={logo} />
                 <div className="text-base text-black dark:text-white">{title}</div>
                 <div className="text-[#b6cdef]">{article}</div>
-                <div>{url}</div>
             </div>
         </div>)
 }
