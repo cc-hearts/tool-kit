fa62ad27e477ef992c5ef2bea7e32a1a8e9c0457
Carl Chen <71313168+cc-hearts@users.noreply.github.com>
2024-03-13 23:52:13 +0800

    perf: optimize pages (#2)
    * wip: wip page-word
    
    * fix: fix bug
    
    * perf: optimize pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import ToolCard from "@/components/tool-card/tool-card";
 import Provider from "@/app/provider";
 import React from "react";
 
-
 export default function Page() {
     const [state] = React.useState([{
         title: 'word transition',
+        logo: './img/word-translation-logo.png',
         article: 'words are translated into different styles of words',
         url: '/word-translation'
     }])
+
     return <Provider>
         <div className="h-screen">
-            <div className="grid grid-cols-3 gap-3 p-12">
-                {state.map((target, index) => <ToolCard key={index} {...target}>Click me</ToolCard>)}
+            <div className="grid grid-cols-3 gap-3 p-12 max-w-[1200px] min-w-[1000px] m-auto">
+                {state.map((target, index) => <ToolCard key={index} {...target}></ToolCard>)}
             </div>
         </div>
     </Provider>
--- src/components/tool-card/tool-card.tsx
+++ src/components/tool-card/tool-card.tsx
@@ -1,31 +1,33 @@
-import {Avatar} from "@nextui-org/react";
+import { Avatar } from "@nextui-org/react";
 import React from "react";
-import {useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 interface Props {
     avatar?: string;
     title?: string
     article?: string
     url?: string
+    logo?: string
 }
 
-const ToolCard: React.FC<React.PropsWithChildren<Props>> = ({title, article, url}) => {
+const ToolCard: React.FC<React.PropsWithChildren<Props>> = ({ logo, title, article, url }) => {
     const router = useRouter()
     return (
-        <div className="bg-gray-400 dark:border-gray-400 overflow-auto rounded-xl cursor-pointer" onClick={() => {
+        <div className="bg-gray-200 hover:bg-gray-400 transition dark:border-gray-400 overflow-auto rounded-xl cursor-pointer" onClick={() => {
             if (!url) return
+
             if (url.startsWith('http')) {
                 window.open(url)
-            } else {
-                router.push(url)
+                return
             }
 
+            router.push(url)
+
         }}>
-            <div className="flex flex-col gap-2 p-4 text-sm bg-white dark:bg-slate-900 rounded-xl  m-[1px]">
-                <Avatar isBordered radius='full' src="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
+            <div className="flex flex-col gap-2 p-4 text-sm bg-white dark:bg-[#1B1C1D] rounded-xl  m-[1px]">
+                <Avatar isBordered radius='full' src={logo} />
                 <div className="text-base text-black dark:text-white">{title}</div>
-                <div className="text-[#b6cdef]">{article}</div>
-                <div>{url}</div>
+                <div className="text-[#536175]">{article}</div>
             </div>
         </div>)
 }
