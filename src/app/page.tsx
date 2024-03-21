cfb51a5de8a267aea103c69c415ce8892dbda311
Carl Chen <ccheartszzz@gmail.com>
2024-03-22 00:01:59 +0800

    fix: fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+'use client';
 import ToolCard from "@/components/tool-card/tool-card";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type ToolOptions = Array<{ title: string, logo: string, article: string, url: string }>
 
-export default async function Page() {
-    const state: ToolOptions = await (await fetch(process.env.BASE_URL +'/apis')).json()
+export default function Page() {
+    const [state, setState] = useState<ToolOptions[]>([]);
+
+    useEffect(() => {
+        fetch('/apis').then(response => response.json())
+            .then(data => setState(data))
+    }, [])
 
     return <div className="h-screen">
         <div className="grid grid-cols-3 gap-3 p-12 max-w-[1200px] min-w-[1000px] m-auto">
