3a08f028f4267549802b8426f07a6d988b06fb9f
Carl Chen <ccheartszzz@gmail.com>
2024-03-21 22:48:02 +0800

    fix: fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 type ToolOptions = Array<{ title: string, logo: string, article: string, url: string }>
 
 export default async function Page() {
-    const state: ToolOptions = await (await fetch('http://localhost:3000/apis')).json()
+    const state: ToolOptions = await (await fetch(process.env.BASE_URL +'/apis')).json()
 
     return <div className="h-screen">
         <div className="grid grid-cols-3 gap-3 p-12 max-w-[1200px] min-w-[1000px] m-auto">
