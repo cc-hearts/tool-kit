6a150f0603e9f703f15a37e934498c1cf5c0462c
carl-chen <ccheartszzz@gmail.com>
2024-04-22 00:23:00 +0800

    perf: add loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,18 @@ type ToolOptions = Array<{ title: string, logo: string, article: string, url: st
 
 export default function Page() {
     const [state, setState] = useState<ToolOptions[]>([]);
-
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
         fetch('/apis').then(response => response.json())
-            .then(data => setState(data))
+            .then(data => {
+                setState(data)
+                setLoading(true)
+            })
     }, [])
 
-    return <div className="h-screen">
-        <div className="grid grid-cols-3 gap-3 p-12 max-w-[1200px] min-w-[1000px] m-auto">
-            {state.map((target, index) => <ToolCard key={index} {...target}></ToolCard>)}
-        </div>
+    return !loading ? <div className="flex items-center justify-center h-screen">loading...</div> : <div className="h-screen">
+    <div className="grid grid-cols-3 gap-3 p-12 max-w-[1200px] min-w-[1000px] m-auto">
+        {state.map((target, index) => <ToolCard key={index} {...target}></ToolCard>)}
     </div>
+</div>
 }
