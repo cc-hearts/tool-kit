a2ce50ba73c17a0d83549a70ea5582a392e34c0a
carl-chen <ccheartszzz@gmail.com>
2024-10-27 22:55:39 +0800

    chore: add yml-to dts pages

--- src/app/yaml-to-dts/page.tsx
+++ src/app/yaml-to-dts/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import {Textarea} from "@nextui-org/react";
 import {useState} from "react";
 import {parse} from 'yaml'
 import generateTypeDeclaration from '@cc-heart/object-to-declare'
-import  './yaml-to-dts.css'
 
 export default function useYamlToDts() {
     const [yamlCtx, setYamlCtx] = useState<string>('');
@@ -18,8 +16,8 @@ export default function useYamlToDts() {
     } catch (e) {
         console.log('[yaml-to-dts]:' + e?.toString())
     }
-    return <div className="yaml-to-dts-pages flex gap-4 h-dvh w-full items-center justify-center p-6">
-        <Textarea className="h-full" value={yamlCtx} onChange={(e) => setYamlCtx(e.currentTarget.value)} />
-        <Textarea className="h-full" value={dtsCtx} />
+    return <div className="flex gap-4 h-dvh w-full items-center justify-center p-6">
+        <textarea className="w-full h-full" value={yamlCtx} onChange={(e) => setYamlCtx(e.currentTarget.value)} />
+        <textarea className="w-full h-full" value={dtsCtx} />
     </div>
 }
